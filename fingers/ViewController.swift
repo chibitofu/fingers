ba8d92c305d5b4c39bde8a9060b6161eae492b43
chibitofu <erinmoony@gmail.com>
2016-12-15 03:54:11 -0800

    Initial commit.

--- fingers/ViewController.swift
+++ fingers/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toeInput: UITextField!
+    
+    @IBOutlet weak var answer: UILabel!
+    
+    var toes: Float = 0.0
+    
+    @IBAction func subButt(_ sender: Any) {
+        var toeInput2: Float
+        
+        toes = Float(arc4random_uniform(5) + 1)
+        
+        if (toeInput.text != "") {
+            toeInput2 = Float(toeInput.text!)!
+            
+            if toeInput2 == toes {
+                answer.text = "Indubitably!"
+            } else {
+                answer.text = "You are wrong! It was \(Int(toes))."
+            }
+        } else {
+            toeInput2 = 0
+            
+            answer.text = "Not enough toes."
+        }
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        toeInput.text = ""
+        
+        answer.text = ""
+        
+        toes = Float(arc4random_uniform(4) + 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
